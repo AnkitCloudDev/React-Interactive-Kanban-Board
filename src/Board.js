23dfff9912e1b25035df207bd5fb3ffa97fc82f2
unknown <ankitdongre96@gmail.com>
2020-07-18 15:20:54 +0100

    Created Patch File 2

--- src/Board.js
+++ src/Board.js
@@ -5,7 +5,17 @@ import Swimlane from './Swimlane';
 import './Board.css';
 
 export default class Board extends React.Component {
-  
+    componentDidMount() {
+        this.drake = Dragula([
+          this.swimlanes.backlog.current,
+          this.swimlanes.inProgress.current,
+          this.swimlanes.complete.current,
+        ]);
+        this.drake.on('drop', (el, target, source, sibling) => this.updateClient(el, target, source, sibling));
+      }
+       componentWillUnmount() {
+            this.drake.remove();
+          }
   constructor(props) {
     super(props);
     const clients = this.getClients();
@@ -23,6 +33,47 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
+    updateClient(el, target, _, sibling) {
+        // Reverting DOM changes from Dragula
+        this.drake.cancel(true);
+    
+        // Find out which swimlane the Card was moved to
+        let targetSwimlane = 'backlog';
+        if (target === this.swimlanes.inProgress.current) {
+          targetSwimlane = 'in-progress';
+        } else if (target === this.swimlanes.complete.current) {
+          targetSwimlane = 'complete';
+        }
+    
+        // Create a new clients array
+        const clientsList = [
+          ...this.state.clients.backlog,
+          ...this.state.clients.inProgress,
+          ...this.state.clients.complete,
+        ];
+        const clientThatMoved = clientsList.find(client => client.id === el.dataset.id);
+        const clientThatMovedClone = {
+          ...clientThatMoved,
+          status: targetSwimlane,
+        };
+    
+        // Remove ClientThatMoved from the clientsList
+        const updatedClients = clientsList.filter(client => client.id !== clientThatMovedClone.id);
+    
+        // Place ClientThatMoved just before the sibling client, keeping the order
+        const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
+        updatedClients.splice(index === -1 ? updatedClients.length : index , 0, clientThatMovedClone);
+    
+        // Update React state to reflect changes
+        this.setState({
+          clients: {
+            backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
+            inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
+            complete: updatedClients.filter(client => client.status && client.status === 'complete'),
+          }
+        });
+      }
+
   getClients() {
     return [
       ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'backlog'],
@@ -57,26 +108,20 @@ export default class Board extends React.Component {
       <Swimlane name={name} clients={clients} dragulaRef={ref}/>
     );
   }
-  dragulaDecorator = (componentBackingInstance) => {
-    if (componentBackingInstance) {
-      let options = { };
-      Dragula([componentBackingInstance], options);
-    }
-  };
-
+ 
   render() {
         return (
       <div className="Board">
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-4">
-              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.dragulaDecorator)}
+              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog)}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.dragulaDecorator)}
+              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress)}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('Complete', this.state.clients.complete, this.dragulaDecorator)}
+              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete)}
             </div>
           </div>
         </div>
